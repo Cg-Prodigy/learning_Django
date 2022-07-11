a5c58b4c52b17d6227af0bd064a7f76af128ef60
Cg-Prodigy <brightmarshall2@gmail.com>
2022-07-11 13:55:15 +0300

    Model and media files

--- media_app/models.py
+++ media_app/models.py
@@ -1,6 +1,17 @@
+from datetime import datetime
 from django.db import models
 
+# utility functions
+def generate_folder(instance,filename):
+    date_time=datetime.now().strftime("%Y%M%d%H%M%S")
+    return "{}/{}/{}".format(date_time,instance.username,filename)
 # Create your models here.
 class LandLord(models.Model):
     first_name=models.CharField(max_length=20, verbose_name='First Name')
     prof_pic=models.ImageField()
+
+class Tenant(models.Model):
+    first_name=models.CharField(max_length=20,verbose_name="First Name")
+    last_name=models.CharField(max_length=20, verbose_name='Last Name')
+    username=models.CharField(unique=True)
+    profile_pic=models.FileField(upload_to=generate_folder)
